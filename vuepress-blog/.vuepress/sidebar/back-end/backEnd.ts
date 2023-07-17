4b6eee271fd068f1a926492855943e1e5186bcdf
xumingyu2018 <260602448@qq.com>
2023-07-17 22:38:04 +0800

    测试bug

--- vuepress-blog/.vuepress/sidebar/back-end/backEnd.ts
+++ vuepress-blog/.vuepress/sidebar/back-end/backEnd.ts
@@ -4,26 +4,21 @@ export const backEnd = arraySidebar([
   {
     text: 'Java',
     icon: 'java',
-    collapsable: true,
     children: [
       {
         text: '基础',
-        collapsable: true,
         children: ['Java-basic'],
       },
       {
         text: '容器',
-        collapsable: true,
         children: ['Java-collection'],
       },
       {
         text: '线程',
-        collapsable: true,
         children: ['Java-thread'],
       },
       {
         text: '新特性',
-        collapsable: true,
         children: ['Java8-new-features'],
       },
     ],
@@ -33,16 +28,13 @@ export const backEnd = arraySidebar([
     text: '数据库',
     icon: 'storage',
     prefix: 'database/',
-    collapsable: true,
     children: [
       {
         text: 'MySQL',
-        collapsable: true,
         children: ['mysql'],
       },
       {
         text: 'Redis',
-        collapsable: true,
         children: [],
       },
     ],
